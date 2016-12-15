70f0d5892972c3f75b2886b853c41ce5a37dd6d7
jaelliott <ell13010@byui.edu>
2016-12-15 07:44:49 -0700

    finisd

--- final/java/5_day.js
+++ final/java/5_day.js
@@ -1,1 +1,123 @@
-https: //api.nasa.gov/predictthesky/apod?api_key=9Q9lD07SLuf4ZWyRX8vpjgIGdnCxg4j29Pupfd4O
+$(function () {
+
+
+    var status = $('#status');
+
+    (function getGeoLocation() {
+        status.text('Getting Location...');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var lon = position.coords.longitude;
+
+                // Call the getData function, send the lat and long
+                getData(lat, lon);
+
+            });
+        } else {
+            status.text("Your browser doesn't support Geolocation or it is not enabled!");
+        }
+
+    })();
+
+    // Get the data from the wunderground API
+    function getData(lat, lon) {
+        var cityDisplay = $('.cityDisplay');
+
+        $.ajax({
+            url: "https://api.wunderground.com/api/9fc78788ceaa4015/geolookup/astronomy/q/" + lat + "," + lon + ".json",
+            dataType: "jsonp",
+            success: function (parsed_json) {
+                var location1 = parsed_json['location']['city'];
+                var location2 = parsed_json['location']['state'];
+                var location = location1 + ", " + location2;
+                var percent_illuminated = parsed_json['moon_phase']['percentIlluminated'];
+                var ageOfMoon = parsed_json['moon_phase']['ageOfMoon'];
+                var sunset1 = parsed_json['moon_phase']['sunset']['hour'];
+                var sunset2 = parsed_json['moon_phase']['sunset']['minute'];
+                var sunset = sunset1 + ":" + sunset2;
+                $("#cityDisplay").text(location);
+                $("#percentIlluminated").text("The percent illuminated is  " + percent_illuminated);
+
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon').html(img);
+
+                if (ageOfMoon == 17) {
+                    ageOfMoon1 = 1;
+                } else {
+                    ageOfMoon1 = ageOfMoon + 1
+                };
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon1').html(img)
+                if (ageOfMoon == 17) {
+                    ageOfMoon1 = 1;
+                } else {
+                    ageOfMoon1 = ageOfMoon + 1
+                };
+                var img
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon2').html(img)
+
+                if (ageOfMoon == 17) {
+                    ageOfMoon1 = 1;
+                } else {
+                    ageOfMoon1 = ageOfMoon + 1
+                };
+                var img
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon3').html(img)
+
+                if (ageOfMoon == 17) {
+                    ageOfMoon1 = 1;
+                } else {
+                    ageOfMoon1 = ageOfMoon + 1
+                };
+                var img
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon4').html(img)
+
+                if (ageOfMoon == 17) {
+                    ageOfMoon1 = 1;
+                } else {
+                    ageOfMoon1 = ageOfMoon + 1
+                };
+                var img
+                var img = document.createElement("IMG");
+                img.src = "http://icons.wunderground.com/graphics/moonpictsnew/moon" + ageOfMoon + ".gif"
+                $('#ageOfMoon5').html(img)
+
+
+
+
+
+
+
+
+
+                $("#sunset").text("Sun set is " + sunset);
+
+            }
+        });
+
+
+
+        cityDisplay.val(location);
+
+
+        $("#cover").fadeOut(250);
+    }
+});
+
+
+
+// A function for changing a string to TitleCase
+function toTitleCase(str) {
+    return str.replace(/\w+/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
---
+++ final/java/pic.js
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+function pic() {
+    var random1 = 1 + (Math.random() * (6 - 1))
+    var random2 = 1 + (Math.random() * (6 - 1))
+    var random3 = 1 + (Math.random() * (6 - 1))
+    var random4 = 1 + (Math.random() * (6 - 1))
+
+}
